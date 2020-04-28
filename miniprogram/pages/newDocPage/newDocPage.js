58686a5c814f3fdbcb6630096fb4aa4aa91bec2c
iris9 <iris918919_ol@outlook.com>
2020-04-28 19:58:20 +0800

    newdoc&newdir

--- miniprogram/pages/newDocPage/newDocPage.js
+++ miniprogram/pages/newDocPage/newDocPage.js
@@ -1,3 +1,35 @@
-Page({
+const app = getApp();
 
+const identity = require('../../service/identity')
+const document = require('../../service/document')
+const filesys = require('../../service/filesys')
+
+Page({
+  data:{
+    cwd: null,
+    doc: null
+  },
+  onload:function(){
+    this.setData({
+      cwd: app.globalData.tmp_arg
+    })
+  },
+
+  bindDocNameInput: function(e){
+    this.data.doc = new Docfile(e.detail.value, null, this.data.cwd);
+  },
+
+  newDoc: function(){
+    document.createDocument(this.data.doc.name, this.data.doc.getPath(), null)
+    .then((newDocument, timestamp)=>{
+      wx.showToast({title: "新建文件成功！"+ timestamp});
+      app.globalData.tmp_arg = this.data.doc;
+      wx.navigateBack({//返回
+        delta: 1
+      });
+    })
+    .catch((e) =>{
+      wx.showToast({title: "新建文件失败!"})
+    })
+  }
 })
--- miniprogram/pages/newFolderPage/newFolderPage.js
+++ miniprogram/pages/newFolderPage/newFolderPage.js
@@ -1,3 +1,35 @@
-Page({
+const app = getApp();
 
+const identity = require('../../service/identity')
+const document = require('../../service/document')
+const filesys = require('../../service/filesys')
+
+Page({
+  data:{
+    cwd: null,
+    doc: null
+  },
+  onload:function(){
+    this.setData({
+      cwd: app.globalData.tmp_arg
+    })
+  },
+
+  bindDirNameInput: function(e){
+    this.data.doc = new Dirfile(e.detail.value, this.data.cwd);
+  },
+
+  newDir: function(){
+    document.createDocument(this.data.doc.name, this.data.doc.getPath(), null)
+    .then((newDocument, timestamp)=>{
+      wx.showToast({title: "新建文件成功！"+ timestamp});
+      app.globalData.tmp_arg = this.data.doc;
+      wx.navigateBack({//返回
+        delta: 1
+      });
+    })
+    .catch((e) =>{
+      wx.showToast({title: "新建文件失败!"})
+    })
+  }
 })
