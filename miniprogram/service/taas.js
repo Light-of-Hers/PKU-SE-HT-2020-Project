282b33f7d979854ea07a5a2b758958671c314cbf
HannahYY <49138710+HannahYY@users.noreply.github.com>
2020-05-21 14:01:02 +0800

    Add files via upload

--- miniprogram/service/taas.js
+++ miniprogram/service/taas.js
@@ -3,18 +3,59 @@ const sdk = require("miniprogram-taas-sdk")
 module.exports = {
     credential: null,
     getCredential(invitationCode) {
-
+        return new Promise((resolve, reject)=>{
+            sdk.getCredential(invitationCode, null, (err, data)=>{
+                if(err){
+                    reject(err)
+                }else{
+                    this.credential = data.credential
+                    resolve(this.credential)
+                }
+            })
+        })
     },
     storeEvidence(data) {
-        
+        return new Promise((resolve, reject)=>{
+            sdk.storeEvidence(data, null, this.credential, (err, obj) => {
+                if(err){
+                    reject(err)
+                } else{
+                    resolve(obj)
+                }
+            })
+        })
     },
-    queryEvidence(data) {
-
+    queryEvidence(hashId) {
+        return new Promise((resolve, reject)=>{
+            sdk.queryEvidence(hashId, null, this.credential, (err, obj) => {
+                if(err){
+                    reject(err)
+                } else{
+                    resolve(obj)
+                }
+            })
+        })
     },
     getContractList() {
-
+        return new Promise((resolve, reject)=>{
+            sdk.getContractList(null, this.credential, (err, obj) => {
+                if(err){
+                    reject(err)
+                } else{
+                    resolve(obj)
+                }
+            })
+        })
     },
     executeContract(contractInfo) {
-
+        return new Promise((resolve, reject)=>{
+            sdk.executeContract(contractInfo, null, this.credential, (err, obj) => {
+                if(err){
+                    reject(err)
+                } else{
+                    resolve(obj)
+                }
+            })
+        })
     }
 }
